3f5f9200606491c5aa95eed1e0c15b2e46a66afb
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-10-30 23:42:37 +0200

    m

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,10 +9,11 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slow && fast && fast->next)
+	while (fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
+
 		if (slow == fast)
 			return (1);
 	}
