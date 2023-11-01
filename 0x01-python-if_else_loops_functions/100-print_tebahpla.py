7ea4c62157e9eeda33e06c2da567f605156d98c3
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-01 03:31:27 +0200

    m

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print(
-        "{}".format("".join([chr(122 - i)
-            if i % 2 == 0 else chr(90 - i // 2)
-            for i in range(52)])), end="")
+print("{}".format("".join([chr(122 - i // 2)
+if i % 2 == 0 else chr(90 - i // 2)
+for i in range(52)])), end="")
