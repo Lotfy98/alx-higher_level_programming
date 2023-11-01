a3c33d49ba8b4953e890e0950e4cd6038eefccdc
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-01 02:08:29 +0200

    m

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print(
-        "{}".format(''.join(chr(i) for i in range(97, 123)
-            if chr(i) not in ['q', 'e']))
-        , end="")
+output = ""
+for i in range(97, 123):
+    if i not in [101, 113]:
+        output += chr(i)
+print("{}".format(output), end="")
