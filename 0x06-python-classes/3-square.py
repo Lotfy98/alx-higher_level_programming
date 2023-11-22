570edee48fea76680cbdaaa5e9478ae4635e52f4
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-23 01:42:48 +0200

    m

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,16 +7,17 @@ class Square:
 
     def __init__(self, size=0):
         """Constructor.
-            Args:
+        
+        Args:
             size: Length of a side of the square.
-            Raises:
+            
+        Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
@@ -26,4 +27,4 @@ class Square:
             Returns:
             The size squared.
         """
-        return (self.__self * self.__size)
+        return (self.__self ** 2)
