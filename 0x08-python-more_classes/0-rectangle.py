cff4da9e5e9493d725b66848540b3dfc1276f857
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-28 02:04:26 +0200

    m

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-	"""Defining emtpy rectangle class
-	"""
+"""Defining emtpy rectangle class
+"""
 
 
 class Rectangle:
-	"""Empty rectangle class
-	"""
-	pass
+    """Empty rectangle class
+    """
+    pass
