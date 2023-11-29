e07f839ecd3cc0d36414ea6d4cdb114e6cf2757b
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-11-29 19:31:30 +0200

    m

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,25 +1,28 @@
-#include <Python.h>
+#include "Python.h"
 
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
 void print_python_string(PyObject *p)
 {
- long int length;
+ long int len;
 
- /* Check if p is a string */
- if (!PyUnicode_Check(p))
+ fflush(stdout);
+
+ printf("[.] string object info\n");
+ if (strcmp(p->ob_type->tp_name, "str") != 0)
  {
-		printf("[ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
  }
 
- /* Get the length of the string */
- length = PyUnicode_GET_LENGTH(p);
+ len = ((PyASCIIObject *)(p))->length;
 
- /* Print the string object info */
- printf("[.] string object info\n");
  if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
  else
 		printf("  type: compact unicode object\n");
- printf("  length: %ld\n", length);
- printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+ printf("  length: %ld\n", len);
+ printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
 }
