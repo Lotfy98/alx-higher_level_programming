e831c7315d13796d6b27a44c0dcf7b21c79acb09
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-12-06 21:31:23 +0200

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Student representation module"""
+"""Student module"""
 
 
 class Student:
-    """
-    Defines student representation
-    """
+    """Defines a student with first name, last name, and age attributes."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initialize an student's representation
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initializes a new Student instance with the given names and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
-        Retrieves a dict representation of student
+        Retrieves a dictionary representation of a Student instance.
 
-        Returns:
-            dict: dictionary containing the student's attributes
+        If attrs is a list of strings, only attributes contained in the list
+        are retrieved. Otherwise, all attributes are retrieved.
         """
-        return (self.__dict__)
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+        return self.__dict__
