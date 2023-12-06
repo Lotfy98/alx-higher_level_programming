071aa492daf3766658550fd7a84edb049eb21c0c
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-12-06 21:44:15 +0200

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-reads stdin line by line and computes metrics
-"""
 
 
-import sys
-import signal
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == "__main__":
+    import sys
 
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-def print_stats():
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
+            line = line.split()
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
-signal.signal(signal.SIGINT, signal_handler)
+        print_stats(size, status_codes)
 
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        size = int(parts[-1])
-        status_code = int(parts[-2])
-        total_size += size
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-
-        if line_count % 10 == 0:
-            print_stats()
-
-except KeyboardInterrupt:
-    pass
-finally:
-    print_stats()
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
