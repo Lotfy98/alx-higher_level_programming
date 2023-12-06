c09e5c46d7b142a83ce44f46d039bd61b088d214
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-12-06 20:13:36 +0200

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -17,5 +17,5 @@ def save_to_json_file(my_obj, filename):
     Returns:
         None
     """
-    with open(filename,'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
