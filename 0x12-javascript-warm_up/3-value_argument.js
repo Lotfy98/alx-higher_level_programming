4e9181129f485e495438990c6e215f627b330dc6
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-02-13 01:37:35 +0200

    m

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-
-if (firstArg) {
-  console.log(firstArg);
-} else {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
