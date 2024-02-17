d43f64ed1808ba713b002df44489d9f80cf88d78
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-02-18 00:49:55 +0200

    m

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,14 +2,14 @@
 const SquareP = require('./5-square');
 
 class Square extends SquareP {
-  charprint (x) {
-    if (x === undefined) {
-      x = 'X';
+  charprint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
     for (let i = 0; i < this.hegight; i++) {
       let s = '';
       for (let y = 0; y < this.width; y++) {
-        s += x;
+        s += c;
       }
       console.log(s);
     }
