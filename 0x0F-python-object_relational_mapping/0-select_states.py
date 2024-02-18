5aee489e94580597bbfe16a8d7f694cd6492b789
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-02-19 00:36:29 +0200

    m

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""script that takes in arguments and displays all values in the states
-"""
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name  = %(sql_name)s ORDER BY id",
-                {'sql_name': sys.argv[4]})
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
