4287a6696867e141040468398de867cb64c7237f
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-03-27 00:59:37 +0200

    m

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-//a script that reads and prints the content of a file
+// a script that reads and prints the content of a file
 const fs = require('fs');
 
 const readFile = (filePath) => {
-	try {
-		return fs.readFileSync(filePath, 'utf-8');
-	} catch (err) {
-		return err;
-	}
-}
+  try {
+    return fs.readFileSync(filePath, 'utf-8');
+  } catch (err) {
+    return err;
+  }
+};
 
 const filePath = process.argv[2];
 
 const content = readFile(filePath);
 
 if (content instanceof Error) {
-	console.log(content);
-}
-else {
-	console.log(content);
+  console.log(content);
+} else {
+  console.log(content);
 }
